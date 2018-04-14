f32efa6a8129e2a4a15b25f32dd1674774e83212
GenialX <admin@ihuxu.com>
2018-04-14 23:56:49 +0800

    :bug: cannot concatenate str and int

--- library/monitor/rpi.py
+++ library/monitor/rpi.py
@@ -41,7 +41,7 @@ def set_sound_off():
 
 def send_cmd_by_ir_remote(cmd):
     status, output = commands.getstatusoutput("irsend SEND_ONCE /home/pi/lircd.conf " + cmd)
-    logging.info("[library.monitor.rpi:send_cmd_by_ir_remote] execute cmd: irsend SEND_ONCE /home/pi/lircd.conf " + cmd + " status:" + status)
+    logging.info("[library.monitor.rpi:send_cmd_by_ir_remote] execute cmd: irsend SEND_ONCE /home/pi/lircd.conf " + cmd + " status:" + str(status))
     if status != 0:
         logging.warning("[library.monitor.rpi:send_cmd_by_ir_remote] output:" + output)
     return status
