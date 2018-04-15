47e60f3438e4b8ad4fd875ec84fae20425d9135e
GenialX <admin@ihuxu.com>
2018-04-15 20:41:46 +0800

    :bug: response data

--- library/brain/monitor.py
+++ library/brain/monitor.py
@@ -11,6 +11,6 @@ def execute(msg):
     data["bulb_status"] = yeelight.get_bulb_info(yeelight.IDX_YEELIGHT_BEDROOM_LIGHT)
     data["light_model_status"] = rpi.get_light_data()
     result = message.get_common_msg()
-    result["message_type"] = message.MESSAGE_TYPE_RPI_DATA_HOME_DEVICE_INFO
+    data["message_type"] = message.MESSAGE_TYPE_RPI_DATA_HOME_DEVICE_INFO
     result["data"] = data
     server.writeline(json.dumps(result))
