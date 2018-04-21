5a7a13647e78a51e729f4075f35d6a0ad23fc4e8
GenialX <admin@ihuxu.com>
2018-04-21 16:30:48 +0800

    :art: art

--- library/brain/monitor.py
+++ library/brain/monitor.py
@@ -3,9 +3,9 @@ import logging, json
 from hestiarpi.library.monitor import yeelight
 from hestiarpi.library.monitor import rpi
 from hestiarpi.model import message
+import hestiarpi.library.server
 
 def execute(msg):
-    from hestiarpi.library.server import server
     logging.info("[library.brain.monitor:execute] msg:" + msg)
     data = {}
     data["bulb_status"] = yeelight.get_bulb_info(yeelight.IDX_YEELIGHT_BEDROOM_LIGHT)
@@ -13,4 +13,4 @@ def execute(msg):
     result = message.get_common_msg()
     data["message_type"] = message.MESSAGE_TYPE_RPI_DATA_HOME_DEVICE_INFO
     result["data"] = data
-    server.writeline(json.dumps(result))
+    server.server.writeline(json.dumps(result))
