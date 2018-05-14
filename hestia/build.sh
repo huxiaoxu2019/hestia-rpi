e7548b30d3c58598f18905121132caab06b7b51b
GenialX <admin@ihuxu.com>
2018-05-14 17:57:51 +0800

    :fire: rm dot sh files

--- hestia/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nohup ./main.py > console &
-
-tail -f console log
--- hestia/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pid=`ps -ef | grep "main.py" | grep -v "grep" | awk '{print $2}'`
-if [ $pid -gt 0 ];then
-    `kill -9 ${pid}`
-    echo "hestia rpi(pid ${pid}) has been stopped."
-else
-    echo "no rpi server running."
-fi
